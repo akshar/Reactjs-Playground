4c7c5d59d15a140d8a52b4a0317ee56fed262e0d
Akshar Dave <akshar314@gmail.com>
2016-11-19 12:36:44 +0530

    starting codeschool poweringup with reactjs course :XD

--- app/index.js
+++ app/index.js
@@ -1,62 +1,8 @@
 var React = require('react');
 var ReactDom = require('react-dom');
-
-var USER_DATA ={
-	name : "Akshar Dave",
-	username : "akshar314",
-	imgUrl : "http://avatars2.githubusercontent.com/u/12473863?v=3&s=460"
-}
-
-
-var HelloWorld = React.createClass({
-
-	render : function(){
-		return (
-			<div>Hello {this.props.name}</div>
-			)
-	}
-});
-
-var ProfilePic = React.createClass({
-	render : function() {
-		return  (<img src ={this.props.imageUrl} />)
-	}
-});
-
-var ProfileLink = React.createClass({
-	render : function() {
-		return (
-			<div> 
-			<a href = {"http://github.com/" + this.props.username }>
-			{this.props.username}
-			</a>
-			</div>
-			)
-	}
-});
-
-var ProfileName = React.createClass({
-	render : function() {
-		return (
-		<div>
-		{this.props.name}
-		</div>)
-	}
-
-});
-
-var Avatar = React.createClass({
-	render : function(){
-		return (
-			<div>
-			<ProfilePic imageUrl = {this.props.user.imgUrl} />
-			<ProfileName name = {this.props.user.name} />
-			<ProfileLink username = {this.props.user.username} />
-			</div>)
-	}
-})
+var routes = require("./config/routes");
 
 ReactDom.render(
-	<Avatar user = {USER_DATA} />,
+	routes,
 	document.getElementById('app')
-	)
+);
