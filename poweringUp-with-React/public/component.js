69a5e8925a4f8be3c86a76aa931cc00f94da0aeb
Akshar Dave <akshar314@gmail.com>
2016-11-20 13:17:28 +0530

    [Modify] comment box component to accept dynamic values

--- poweringUp-with-React/public/component.js
+++ poweringUp-with-React/public/component.js
@@ -1,29 +1,24 @@
-// class StoryBox extends React.Component {
-//
-//   render() {
-//     const storylist = ['js','ruby','clojure']
-//     return (<div>Story Box compoenent
-//       <ul>
-//       {storylist.map((item)=><li>{item}</li>)}
-//       </ul>
-//       </div>);
-//     }
-//  }
-
   class CommentBox extends React.Component {
+    _getComments() {
+      const commentList = [{ id: 1, author: 'Morgan McCircuit', body: 'Great picture!' },
+      { id: 2, author: 'Bending Bender', body: 'Excellent stuff' }]
+
+      return commentList.map((comment) => {
+        return (
+          <Comment author ={comment.author} body = {comment.body} key ={comment.id} />
+        );
+      });
+    }
     render(){
+      const comments = this._getComments();
       return (
         <div className ="commet-box">
           <h3>Comments</h3>
-          <h4 className="comment-count">2 Comments</h4>
+          <h4 className="comment-count">{comments.length} comments</h4>
           <div className="comment-list">
-          <Comment author="phoenix" body="lorem ipsum a qu"/>
-          <Comment author="oculus" body="Lorem ipsum dolor sit amet, "/>
+          {comments}
           </div>
         </div>
-
-
-
       );
     }
   }
