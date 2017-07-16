8f3e7a614fc770882241d3d4508d47912982f34d
Akshar Dave <akshar314@gmail.com>
2017-07-16 13:37:33 +0530

    normal syntax

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -1,11 +1,12 @@
 var Greeter = React.createClass({
   render: function() {
-    return (
-      <div>
-        <h1>Hello react</h1>
-        <p>This is from a component</p>
-      </div>
-    );
+       return React.createElement('h1',null,'helloreact');  // normal syntax
+    // return ( // jsx syntax
+    //   <div>
+    //     <h1>Hello react</h1>
+    //     <p>This is from a component</p>
+    //   </div>
+    // );
   }
 });
 
