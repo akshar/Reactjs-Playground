497b712c6be9ee753d4912900f1fb7de208ff164
Akshar Dave <akshar314@gmail.com>
2017-08-06 01:57:40 +0530

    Adds Boilerplate app

---
+++ ReactBoilerlate/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+	entry: './app/app.jsx',
+	output: {
+		path: __dirname,
+		filename: './public/bundle.js'
+	},
+	resolve: {
+		root: __dirname,
+		alias: {
+	
+		},
+		extensions: ['','.js','.jsx']
+	},
+
+	module:  {
+		loaders: [
+		  {
+			loader: 'babel-loader',
+			query: {
+				presets: ['es2015','react']
+			},
+			test: /\.jsx?$/,
+			exclude: /(node_modules|bower_components)/
+		  } 
+		]
+	} 
+};
