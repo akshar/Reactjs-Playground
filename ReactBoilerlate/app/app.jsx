adc9407a7c072a593f7292e70543accc7a8f8ca4
Akshar Dave <akshar314@gmail.com>
2017-08-06 16:07:48 +0530

    * Weather app boilerplate * Setting up react router

--- ReactBoilerlate/app/app.jsx
+++ ReactBoilerlate/app/app.jsx
@@ -1,6 +1,18 @@
 var React       =   require('react');
 var ReactDOM    =   require('react-dom');
 
+var objOne = {
+  name: "Akshar",
+  location: "Bangalore"
+}
+
+var objTwo = {
+  last: "Dave",
+  ...objOne //spread operator for combining two objects
+}
+
+console.log(objTwo);
+
 ReactDOM.render(
   <h1>Boilerplate App</h1>,
   document.getElementById('app')
